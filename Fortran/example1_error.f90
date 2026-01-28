1fdb0fdc77aebd3d68cfb602a1bf573fb9fd28b5
Gaurav Bhutani <gaurav@iitmandi.ac.in>
2026-01-28 19:58:38 +0530

    Basic Fortran codes

---
+++ Fortran/example1_error.f90
@@ -0,0 +1,12 @@
+
+program example1
+
+!    implicit none
+    real (kind=8) :: x,y,z
+
+    x = 3.d0
+    y = 2.d-1
+    zz = x + y
+    print *, "z = ", z
+
+end program example1
---
+++ Fortran/program1.f90
@@ -0,0 +1,12 @@
+
+program program1
+
+    implicit none
+    real (kind=8) :: x,y,z
+
+    x = 1.d0
+    y = 3.d-1
+    z = x + y
+    print *, "z = ", z
+
+end program program1
